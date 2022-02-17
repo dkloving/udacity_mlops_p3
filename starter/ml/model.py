949397d1d0e59db1f9ab58691db9bae5761788c0
David K Loving <dkloving@users.noreply.github.com>
2022-02-17 11:23:27 -0600

    initial model training and saving

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.ensemble import AdaBoostClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -17,8 +17,8 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-
-    pass
+    trained_model = AdaBoostClassifier().fit(X_train, y_train)
+    return trained_model
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +57,5 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict(X)
+    return preds
--- starter/train_model.py
+++ starter/train_model.py
@@ -1,12 +1,25 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+import pandas as pd
+import joblib
+import logging
 
 # Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model
+
+# setup logger
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 # Add code to load in the data.
+logger.info("Reading data")
+data = pd.read_csv("data/census_clean.txt")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
+logger.info("Preprocessing data")
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -19,10 +32,28 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
+logger.info("Training Model")
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+                                    train,
+                                    categorical_features=cat_features,
+                                    label="salary",
+                                    training=True
+                                )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+                                    test,
+                                    categorical_features=cat_features,
+                                    label="salary",
+                                    training=False,
+                                    encoder=encoder,
+                                    lb=lb
+                                )
 
 # Train and save a model.
+save_dest = 'model/trained_pipeline.pkl'
+logger.info("Saving pipeline to %s", save_dest)
+model = train_model(X_train, y_train)
+pipeline = make_pipeline(encoder, model)
+joblib.dump(pipeline, save_dest)
