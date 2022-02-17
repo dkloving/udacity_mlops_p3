5e8ee79211f29b6377385a679a172698f07599b8
David K Loving <dkloving@users.noreply.github.com>
2022-02-17 11:35:26 -0600

    refactored code to be a little more acceptable

--- starter/train_model.py
+++ starter/train_model.py
@@ -14,46 +14,56 @@ from ml.model import train_model
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
-# Add code to load in the data.
-logger.info("Reading data")
-data = pd.read_csv("data/census_clean.txt")
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-logger.info("Preprocessing data")
-train, test = train_test_split(data, test_size=0.20)
+def prepare_data():
+    logger.info("Reading data")
+    data = pd.read_csv("data/census_clean.txt")
+    
+    logger.info("Preprocessing data")
+    train, test = train_test_split(data, test_size=0.20)
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+                                        train,
+                                        categorical_features=cat_features,
+                                        label="salary",
+                                        training=True
+                                    )
+    X_test, y_test, encoder, lb = process_data(
+                                        test,
+                                        categorical_features=cat_features,
+                                        label="salary",
+                                        training=False,
+                                        encoder=encoder,
+                                        lb=lb
+                                    )
+    return X_train, y_train, X_test, y_test, encoder
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
 
-logger.info("Training Model")
-X_train, y_train, encoder, lb = process_data(
-                                    train,
-                                    categorical_features=cat_features,
-                                    label="salary",
-                                    training=True
-                                )
+def fit_model(X_train, y_train):
+    logger.info("Training Model")
+    model = train_model(X_train, y_train)
+    return model
 
-# Proces the test data with the process_data function.
-X_test, y_test, encoder, lb = process_data(
-                                    test,
-                                    categorical_features=cat_features,
-                                    label="salary",
-                                    training=False,
-                                    encoder=encoder,
-                                    lb=lb
-                                )
 
-# Train and save a model.
-save_dest = 'model/trained_pipeline.pkl'
-logger.info("Saving pipeline to %s", save_dest)
-model = train_model(X_train, y_train)
-pipeline = make_pipeline(encoder, model)
-joblib.dump(pipeline, save_dest)
+def save_model(save_dest, pipeline_objs=[]):
+    # Train and save a model.
+    logger.info("Saving pipeline to %s", save_dest)
+    pipeline = make_pipeline(*pipeline_objs)
+    joblib.dump(pipeline, save_dest)
+
+if __name__ == '__main__':
+    X_train, y_train, _, _, encoder = prepare_data()
+    model = fit_model(X_train, y_train)
+    
+    save_dest = 'model/trained_pipeline.pkl'
+    pipeline_objs = [encoder, model]
+    save_model(save_dest, pipeline_objs)
