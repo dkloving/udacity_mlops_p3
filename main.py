1d1146cf9d91a60f5cd204967bf306255e5f6588
David K Loving <dkloving@users.noreply.github.com>
2022-02-24 16:09:18 -0600

    initial version of API

--- main.py
+++ main.py
@@ -1,1 +1,64 @@
-# Put the code for your API here.
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+
+
+app = FastAPI()
+model = None
+
+
+class DataSample(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 39,
+                "workclass": "State-gov",
+                "fnlgt": 77516,
+                "education": "Bachelors",
+                "education_num": 13,
+                "marital_status": "Never-married",
+                "occupation": "Adm-clerical",
+                "relationship": "Not-in-family",
+                "race": "White",
+                "sex": "Male",
+                "capital_gain": 2174,
+                "capital_loss": 0,
+                "hours_per_week": 40,
+                "native_country": "United-States",
+            }
+        }
+
+
+@app.on_event("startup")
+def load_model():
+    global model
+    model = joblib.load("model/trained_pipeline.joblib")
+
+
+@app.get("/")
+async def read_root():
+    return {"greeting": "Hai! OwO"}
+
+
+@app.post("/infer/")
+async def infer(sample: DataSample):
+    df = pd.DataFrame.from_dict([sample.dict()])
+    pred = model.predict(df)
+    print(pred)
+    return {"model": str(pred)}
