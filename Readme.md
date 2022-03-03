fd97162561a39e45aa9b1ae6dcdba5cd38d71360
David K Loving <dkloving@users.noreply.github.com>
2022-03-03 15:00:40 -0600

    finished adding documentation

---
+++ Readme.md
@@ -0,0 +1,42 @@
+## Public Repo Link
+[Here](https://github.com/dkloving/udacity_mlops_p3)
+
+
+## Git and DVC
+See Github Actions in public repo.
+
+
+Note that files comitted to dvc include a single sklearn pipeline which includes both the trained classifier and onehot encoder.
+
+
+![DVC DAG](dvc_dag.png)
+
+
+## Model Building
+
+I have totally reworked the project starter code. All model-building code is in the `ml` folder.
+
+
+Model file, slice results, and card are all in the `model` folder.
+
+
+## API Creation
+
+Local tests are in `api_tests.py`
+
+
+![API Example](example.png)
+
+
+## API Deployment
+
+Tests for deployed api are in `heroku_test.py`. These can be executed with `pytest heroku_test.py`.
+
+
+The app is live at `https://gentle-thicket-08491.herokuapp.com/`
+
+
+![API Example](live_get.png)
+
+
+![API Example](live_put.png)
--- heroku_test.py
+++ heroku_test.py
@@ -1,64 +1,67 @@
 import requests
 
 
-APP_URL = 'https://gentle-thicket-08491.herokuapp.com/'
+APP_URL = "https://gentle-thicket-08491.herokuapp.com/"
 
 
 def test_api_get_root():
     r = requests.get(APP_URL)
     assert r.status_code == 200
     assert r.json() == {"greeting": "Hai! OwO"}
+    return r.status_code, r.json()
 
 
 def test_get_negative_prediction():
-        r = requests.post(
-            f"{APP_URL}infer/",
-            json={
-                "age": 39,
-                "workclass": "State-gov",
-                "fnlgt": 77516,
-                "education": "Bachelors",
-                "education_num": 13,
-                "marital_status": "Never-married",
-                "occupation": "Adm-clerical",
-                "relationship": "Not-in-family",
-                "race": "White",
-                "sex": "Male",
-                "capital_gain": 2174,
-                "capital_loss": 0,
-                "hours_per_week": 40,
-                "native_country": "United-States",
-            },
-        )
-        assert r.status_code == 200
-        assert r.json() == {"prediction": "<=50k"}
+    r = requests.post(
+        f"{APP_URL}infer/",
+        json={
+            "age": 39,
+            "workclass": "State-gov",
+            "fnlgt": 77516,
+            "education": "Bachelors",
+            "education_num": 13,
+            "marital_status": "Never-married",
+            "occupation": "Adm-clerical",
+            "relationship": "Not-in-family",
+            "race": "White",
+            "sex": "Male",
+            "capital_gain": 2174,
+            "capital_loss": 0,
+            "hours_per_week": 40,
+            "native_country": "United-States",
+        },
+    )
+    assert r.status_code == 200
+    assert r.json() == {"prediction": "<=50k"}
+    return r.status_code, r.json()
 
 
 def test_get_positive_prediction():
-        r = requests.post(
-            f"{APP_URL}infer/",
-            json={
-                "age": 39,
-                "workclass": "State-gov",
-                "fnlgt": 77516,
-                "education": "Bachelors",
-                "education_num": 13,
-                "marital_status": "Never-married",
-                "occupation": "Adm-clerical",
-                "relationship": "Not-in-family",
-                "race": "White",
-                "sex": "Male",
-                "capital_gain": 217400,  # this is the only thing changed lol
-                "capital_loss": 0,
-                "hours_per_week": 40,
-                "native_country": "United-States",
-            },
-        )
-        assert r.status_code == 200
-        assert r.json() == {"prediction": ">50k"}
+    r = requests.post(
+        f"{APP_URL}infer/",
+        json={
+            "age": 39,
+            "workclass": "State-gov",
+            "fnlgt": 77516,
+            "education": "Bachelors",
+            "education_num": 13,
+            "marital_status": "Never-married",
+            "occupation": "Adm-clerical",
+            "relationship": "Not-in-family",
+            "race": "White",
+            "sex": "Male",
+            "capital_gain": 217400,  # this is the only thing changed lol
+            "capital_loss": 0,
+            "hours_per_week": 40,
+            "native_country": "United-States",
+        },
+    )
+    assert r.status_code == 200
+    assert r.json() == {"prediction": ">50k"}
+    return r.status_code, r.json()
 
 
-if __name__ == '__main__':
-    test_api_get_root()
-    test_get_negative_prediction()
-    test_get_positive_prediction()
+if __name__ == "__main__":
+    print(test_api_get_root())
+    print(test_get_negative_prediction())
+    print(test_get_positive_prediction())
